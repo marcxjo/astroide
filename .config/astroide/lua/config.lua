9c43bfcde65858487b34e23eb1a051836f8dd4ce
Marko Givens <marcxjo@cerebralbleach.com>
2025-07-28 19:20:16 -0600

    config: force transparent background

--- .config/astroide/lua/config.lua
+++ .config/astroide/lua/config.lua
@@ -1,2 +1,8 @@
 -- User specific settings not handled by plugins or prior configs
 vim.api.nvim_set_option_value("colorcolumn", "80", {})
+
+-- transparent background
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
