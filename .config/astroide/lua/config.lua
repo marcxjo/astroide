3c9b32a83bfad396d57eebe0adfc34a8990abf93
Marko Givens <marcxjo@cerebralbleach.com>
2026-09-11 18:36:32 -0600

    config.lua: text alignment tweaks; set custom app title

--- .config/astroide/lua/config.lua
+++ .config/astroide/lua/config.lua
@@ -1,8 +1,11 @@
 -- User specific settings not handled by plugins or prior configs
-vim.api.nvim_set_option_value("colorcolumn", "80", {})
+vim.api.nvim_set_option_value("colorcolumn", "81", {})
+vim.api.nvim_set_option_value("textwidth", 80, {})
 
 -- transparent background
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
 vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
+
+vim.api.nvim_set_option_value("titlestring", '%{substitute(getcwd(), "^.*/", "", "")} - AstroIDE', {})
