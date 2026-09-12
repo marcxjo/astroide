de9c7cd55c7a8be9cac67aa75aa828a27a2ca43d
Marko Givens <marcxjo@cerebralbleach.com>
2026-09-11 18:53:17 -0600

    community.lua - add Nordic theme, Nix language support

--- .config/astroide/lua/community.lua
+++ .config/astroide/lua/community.lua
@@ -3,11 +3,13 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  { import = "astrocommunity.colorscheme.nordic-nvim" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.cs" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.nix" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.typescript" },
